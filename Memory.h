7b79c09de7eeb724a63edf31159e3c5c89c9b2f8
yocbar <39558923+yocbar@users.noreply.github.com>
2018-05-23 15:10:49 +0200

    Update Memory.h

--- Memory.h
+++ Memory.h
@@ -1,4 +1,73 @@
+
 #ifndef Memory_h
 #define Memory_h
 
 #include "Plateau.h"
+#include "bool.h"
+#include "etatJeu.h"
+#include "int.h"
+
+class Carte;
+
+class Memory {
+
+ public:
+
+    virtual int getTempsMax();
+
+    virtual int tempsRestant();
+
+    virtual void setEtat(etatJeu UnEtatMemory);
+
+    virtual void setTempsMax(int tempsMax);
+
+    virtual void setTempsRestant(int tempsRestant);
+
+    virtual etatJeu getEtatJeu();
+
+    virtual Plateau etLePlateau();
+
+    virtual int getNbCarteUtilisee();
+
+    virtual bool evaluerCartes();
+
+    virtual void demarrerPartie();
+
+    virtual void abandonnerPartie();
+
+    virtual void terminerPartiePlusDeTemps();
+
+    virtual void terminerPartiePlusDeCarte();
+
+    virtual void setNbCarteUtilisee();
+
+    virtual void setPaireTrouvee(int nbPaireTrouvee);
+
+    virtual void getPaireTrouvee();
+
+    virtual void setCoups(int nbCoups);
+
+    virtual void getCoups();
+
+
+ protected:
+    etatJeu etatJeu;
+    int tempsMax;
+    /* {deprecated=false}*/
+    int tempsRestant;
+    int nbCarteARassembler;
+    int nbCarteUtilisee;
+    int nbPaireTrouvee;
+    int nbCoups;
+
+ public:
+
+    /**
+     * @element-type Carte
+     */
+    Carte *myCarte[ 0];
+
+    Plateau composition;
+};
+
+#endif // Memory_h
