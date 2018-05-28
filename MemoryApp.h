5d24477db846fe0319e5a55e571c7c8de43eff42
yocvito <39558632+yocvito@users.noreply.github.com>
2018-05-28 16:08:36 +0200

    Update MemoryApp.h

--- MemoryApp.h
+++ MemoryApp.h
@@ -1,4 +1,15 @@
 #ifndef APP_H
 #define APP_H
 
+#include <wx/wx.h> 
+
+//définition de la class "Appli"
+class MemoryAppli : public wxApp
+{
+public:
+//appel au démarrage de l'application
+virtual bool OnInit();
+};
+
+DECLARE_APP(MemoryAppli)
 #endif //APP_H
