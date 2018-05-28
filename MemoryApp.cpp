0f19a9477538f4a01e2e989eda20b6eaba34eb8b
yocvito <39558632+yocvito@users.noreply.github.com>
2018-05-28 16:12:02 +0200

    Update MemoryApp.cpp

--- MemoryApp.cpp
+++ MemoryApp.cpp
@@ -1,1 +1,12 @@
 #include "MemoryApp.h"
+#include "MemoryPrincipale.h"
+#include "Memory.h"
+
+//création de l'instance d'application
+IMPLEMENT_APP(MemoryAppli)
+  
+//implémentation méthode OnInit()
+bool Appli::OnInit()
+{
+  //création de la fenetre principale
+  
