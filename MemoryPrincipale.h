965efb4c982bece74092d775431096386b720520
yocbar <39558923+yocbar@users.noreply.github.com>
2018-05-28 16:10:08 +0200

    Update MemoryPrincipale.h

--- MemoryPrincipale.h
+++ MemoryPrincipale.h
@@ -1,5 +1,17 @@
 #ifndef PRINCIPALE_H
 #define PRINCIPALE_H
 
+#include <wx/wx.h>
+
+class MemoryPrincipale : public wxFrame
+{
+  public : 
+  //constructeur
+    MemoryPrincipale(const wxString&);
+    
+    
+  //destructeur
+    virtual ~MemoryPrincipale();
+}
 
 #endif //PRINCIPALE_H
