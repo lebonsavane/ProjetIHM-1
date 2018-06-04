326a3453867452c23870a283031b24c04a3f65a2
yocvito <39558632+yocvito@users.noreply.github.com>
2018-06-04 11:21:59 +0200

    update

--- MemoryPrinicpale.cpp
+++ MemoryPrinicpale.cpp
@@ -1,3 +1,10 @@
 #include "Memory.h"
 #include "MemoryPrincipale.h"
 
+
+
+
+SizerBtn = new wxBoxSizer(wxHORIZONTAL);
+SizerLbl = new wxBoxSizer(wxHORIZONTAL);
+SizerScoreboard = new wxBoxSizer(wxHORIZONTAL);
+
