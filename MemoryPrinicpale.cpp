7207456171d5d21f2620bafbbc9d2df2affec203
yocvito <39558632+yocvito@users.noreply.github.com>
2018-06-04 11:58:38 +0200

    update

--- MemoryPrinicpale.cpp
+++ MemoryPrinicpale.cpp
@@ -13,4 +13,10 @@ SizerMain = new wxBoxSizer(wxVERTICAL);
 
 SizerButton -> Add (BtnD&A, 0, wxALIGN_RIGHT, 5);
 SizerScore -> Add (lblTempsR, 1, wxALIGN_LEFT, 5);
-SizerScore -> Add (lblPaire
+SizerScore -> Add (tempsR,1,wxALIGN_LEFT,5);
+SizerScore -> Add (lblPaires, 1, wxALIGN_LEFT, 5);
+SizerScore -> Add (paires, 1, wxALIGN_LEFT, 5);
+SizerScore -> Add (lblCoups, 1, wxALIGN_LEFT, 5);
+SizerScore -> Add (coups, 1, wxALIGN_LEFT, 5);
+SizerScore -> Add (lblTempsR, 1, wxALIGN_LEFT, 5);
+SizerScore -> Add (tempsR, 1, wxALIGN_LEFT, 5);
