61e177771eca7f95d4f3adec1de3386182486a7e
yocbar <39558923+yocbar@users.noreply.github.com>
2018-06-06 11:34:25 +0200

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -1,5 +1,5 @@
 #include "Memory.h"
-
+/*
 void Memory::actualisation()
 {
 while(getEtatJeu()==Demarrer)
@@ -14,4 +14,87 @@ while(getEtatJeu()==Demarrer)
 }
 } 
 } 
+}*/
+      
+int Memory::getTempsMax()
+int Memory::tempsRestant()
+   
+virtual void Memory::setEtat(etatJeu UnEtatMemory)
+{
+   
+}
+
+virtual void Memory::setTempsMax(int tempsMax)
+{
+
+}
+
+virtual void Memory::setTempsRestant(int tempsRestant)
+{
+
+}
+
+virtual etatJeu Memory::getEtatJeu()
+{
+ return etatJeu;  
+}
+
+virtual Plateau Memory::etLePlateau()
+{
+ return 
+}
+
+virtual int Memory::getNbCarteUtilisee()
+{
+   return nbCarteUtilisee;
+}
+
+virtual bool Memory::evaluerCartes()
+{
+   return false;
+}
+
+virtual void Memory::demarrerPartie()
+{
+  
+}
+
+virtual void Memory::abandonnerPartie()
+{
+
+}
+
+virtual void Memory::terminerPartiePlusDeTemps()
+{
+
+}
+
+virtual void Memory::terminerPartiePlusDeCarte()
+{
+
+}
+
+virtual void Memory::setNbCarteUtilisee()
+{
+
+}
+
+virtual void Memory::setPaireTrouvee(int nbPaireTrouvee)
+{
+
+}
+
+virtual void Memory::getPaireTrouvee()
+{
+
+}
+
+virtual void Memory::setCoups(int nbCoups)
+{
+
+}
+
+virtual void Memory::getCoups()
+{
+
 }
